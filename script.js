b700a0c7dc8b8023611cf1d4257bc0b00431fcbe
Vitaliy <bullavitaliy@gmil.com>
2022-02-08 19:04:59 +0200

    create basic functionality

---
+++ script.js
@@ -0,0 +1,37 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+
+document.addEventListener("keydown", function () {
+  jump();
+});
+
+function jump() {
+  if (dino.classList != "jump") {
+    dino.classList.add("jump");
+  }
+  setTimeout(function () {
+    dino.classList.remove("jump");
+  }, 600);
+}
+
+//score
+let score = document.getElementById("score");
+let mili = 0;
+setInterval(function () {
+  ++mili;
+  console.log(mili);
+  score.textContent = `${mili}`;
+}, 100);
+
+//stop game
+let isAlive = setInterval(function () {
+  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+  let cactusLeft = parseInt(
+    window.getComputedStyle(cactus).getPropertyValue("left")
+  );
+
+  if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+    alert(`GAME OVER! your score: ${mili}`);
+    window.location.reload();
+  }
+}, 10);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+let variable = 0;
+
+setInterval(function() {
+  ++variable;
+}, 1000);
+
+console.log(variable);
